39d46a1c2dec9faf6a29046b1553dff48add803d
kaique <126088480+kaiquealexandre@users.noreply.github.com>
2024-03-06 21:08:23 -0300

    Add files via upload

---
+++ intro jts/1_tipos_primitivos/2_string/escript.js
@@ -0,0 +1,15 @@
+const text1 = "kaique cupin";
+const text2 = "gabizin";
+
+const text3 = "o gabizin foi no banheiro";
+
+console.log(text1);
+console.log(text2);
+console.log(text3);
+
+console.log(text2 + text3);
+const cifrao = '/u0024';
+const aMaiusculo = '/u0041';
+
+console.log(cifrao);
+console.log(aMaiusculo);
