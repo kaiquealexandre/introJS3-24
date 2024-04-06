073ba3a2c405281523e818e0b6e52ea3dddf6632
kaique <126088480+kaiquealexandre@users.noreply.github.com>
2024-04-05 22:05:46 -0300

    Add files via upload

---
+++ introJS3-24-mendes-main/1_tipos_primitivos/1_number/script.js
@@ -0,0 +1,19 @@
+//tipo number
+
+const meuNumero = 24;
+
+const telefone = 44998268318;
+
+const idade = 29;
+
+
+console.log(meuNumero);
+console.log(telefone);
+console.log(idade);
+
+
+const soma = meuNumero + idade;
+
+console.log(soma);
+
+console.log('Node fucioniou')
